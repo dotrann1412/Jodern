529c51bd9faeffa8794b207067b91721ad6f7358
mathesics <huuthien013@gmail.com>
2022-11-24 15:51:02 +0700

    update missing files

---
+++ android/app/src/main/java/android/jodern/app/SplashActivity.java
@@ -0,0 +1,22 @@
+package android.jodern.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed((Runnable) () -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000);
+    }
+}
