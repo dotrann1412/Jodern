aff35a0072fada279de7f2661a42615ec8945c14
mathesics <huuthien013@gmail.com>
2022-11-25 00:29:51 +0700

    update loading animation for splash screen

--- android/app/src/main/java/android/jodern/app/SplashActivity.java
+++ android/app/src/main/java/android/jodern/app/SplashActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
 public class SplashActivity extends AppCompatActivity {
 
