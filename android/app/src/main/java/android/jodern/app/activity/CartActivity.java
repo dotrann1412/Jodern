739d3d145e9a3b6417587ff3fca84e5c1146a822
mathesics <huuthien013@gmail.com>
2022-11-26 21:02:10 +0700

    change layout and test volley API

--- android/app/src/main/java/android/jodern/app/activity/CartActivity.java
+++ android/app/src/main/java/android/jodern/app/activity/CartActivity.java
@@ -12,19 +12,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class CartActivity extends AppCompatActivity {
     private RecyclerView.Adapter adapter;
     private RecyclerView cartRecyclerView;
     private CartController cartController;
 
+    private static String API_URL = null;
+
     TextView subTotalTextView, shippingTextView, totalTextView;
 
     private long total;
-    private LinearLayout cartScrollView;
+    private LinearLayout cartLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +52,7 @@ public class CartActivity extends AppCompatActivity {
 
     private void initView() {
         cartRecyclerView = findViewById(R.id.cartRecyclerView);
-        cartScrollView = findViewById(R.id.cartScrollView);
+        cartLayout = findViewById(R.id.cartLayout);
         subTotalTextView = findViewById(R.id.subTotalValueCartTextView);
         shippingTextView = findViewById(R.id.shippingValueCartTextView);
         totalTextView = findViewById(R.id.totalValueCartTextView);
@@ -61,6 +73,32 @@ public class CartActivity extends AppCompatActivity {
         if (cartController.getCartList() != null && cartController.getCartList().isEmpty()) {
             // TODO do something when cart is empty
         }
+
+
+        API_URL = "http://joderm.store:8000/api/test";
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, API_URL, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+//                            for (int i = 0; i < response.length(); ++i) {
+//                                JSONObject responseObj = response.getJSONObject(i);
+//                                Log.d("VOLLEY", responseObj.toString());
+//                            }
+                            Log.d("VOLLEY", response.toString());
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("VOLLEY", "Error " + error.getMessage());
+                    }
+                });
+//        queue.add(jsonArrayRequest);
     }
 
     @SuppressLint("SetTextI18n")
@@ -72,7 +110,5 @@ public class CartActivity extends AppCompatActivity {
         subTotalTextView.setText(StringUtils.long2money(subTotal));
         shippingTextView.setText(StringUtils.long2money(shippingCost));
         totalTextView.setText(StringUtils.long2money(total));
-
-
     }
 }
