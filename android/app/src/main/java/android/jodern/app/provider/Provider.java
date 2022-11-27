3a15d2cd6f43e1267e31163ec8f166c8da26acda
H.T.Vu <trongvulqd@gmail.com>
2022-11-28 01:38:28 +0700

    update category tags

--- android/app/src/main/java/android/jodern/app/provider/Provider.java
+++ android/app/src/main/java/android/jodern/app/provider/Provider.java
@@ -58,10 +58,10 @@ public class Provider {
     private void initCategories() {
         String[] maleCategories = new String[]{"Áo khoác nam", "Áo thun nam", "Áo polo nam", "Áo sơ mi nam", "Quần jean nam", "Quần tây nam", "Quần kaki nam", "Quần short nam"};
         int[] maleImageIds = new int[]{R.drawable.ao_khoac_nam, R.drawable.ao_thun_nam, R.drawable.ao_polo_nam, R.drawable.ao_so_mi_nam, R.drawable.quan_jean_nam, R.drawable.quan_tay_nam, R.drawable.quan_kaki_nam, R.drawable.quan_short_nam};
-        String[] maleRaws = new String[]{"ao-khoac-nam", "ao-thun-ao-phong-nam", "ao-polo-nam", "ao-so-mi-nam", "quan-jean-nam", "quan-tay-quan-au-nam", "quan-kaki", "quan-short-nam"};
+        String[] maleRaws = new String[]{"ao-khoac-nam", "ao-thun-nam", "ao-polo-nam", "ao-so-mi-nam", "quan-jean-nam", "quan-tay-nam", "quan-kaki", "quan-short-nam"};
         String[] femaleCategories = new String[]{"Áo khoác nữ", "Áo thun nữ", "Áo sơ mi nữ", "Quần jean nữ", "Quần tây nữ", "Quần short nữ", "Chân váy", "Váy đầm"};
         int[] femaleImageIds = new int[]{R.drawable.ao_khoac_nu, R.drawable.ao_thun_nu, R.drawable.ao_so_mi_nu, R.drawable.quan_jean_nu, R.drawable.quan_tay_nu, R.drawable.quan_short_nu, R.drawable.chan_vay, R.drawable.vay_dam};
-        String[] femaleRaws = new String[] {"ao-khoac-nu", "ao-thun-ao-phong-nu", "ao-so-mi-nu", "quan-jean-nu", "quan-au-nu", "quan-short-nu", "chan-vay", "vay-dam"};
+        String[] femaleRaws = new String[] {"ao-khoac-nu", "ao-thun-nu", "ao-so-mi-nu", "quan-jean-nu", "quan-tay-nu", "quan-short-nu", "chan-vay", "vay-dam"};
 
         categoryListMapping.put("nam", new ArrayList<>());
         for (int i = 0; i < maleCategories.length; i++) {
