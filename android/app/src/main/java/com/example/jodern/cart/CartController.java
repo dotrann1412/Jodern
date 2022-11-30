b4faaec9eb10f4a331f5319f262054bacd3fb77c
H.T.Vu <trongvulqd@gmail.com>
2022-12-01 02:56:43 +0700

    complete cart fragment

--- android/app/src/main/java/com/example/jodern/cart/CartController.java
+++ android/app/src/main/java/com/example/jodern/cart/CartController.java
@@ -9,19 +9,41 @@ import android.util.Log;
 import com.example.jodern.cart.cartitem.CartItem;
 import com.example.jodern.cart.cartitem.CartItemDB;
 import com.example.jodern.interfaces.ChangeNumItemsListener;
+import com.example.jodern.model.Product;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class CartController {
     private static final String TAG = CartController.class.getName();
 
     private List<CartItem> cartItemList;
+    private List<Product> productList = new ArrayList<>();
+    private AtomicLong totalPrice;
+
     private CartItemDB cartItemDB;
     private final Context context;
 
-
     private CartController(Context context) {
         this.context = context;
+        try {
+            Log.d(TAG, "CartController: retrieving cart items data");
+            cartItemDB = CartItemDB.with(context);
+            cartItemList = cartItemDB.orderItemDao().loadAll();
+            Log.d(TAG, "CartController: retrieving cart successfully");
+            Log.d(TAG, "Length: " + cartItemList.size());
+        } catch (Exception e) {
+            Log.d(TAG, "CartController: failed to retrieve cart items data");
+            e.printStackTrace();
+        }
+    }
+
+    private CartController(Context context, AtomicLong totalPrice) {
+        this.context = context;
+        this.totalPrice = totalPrice;
         try {
             Log.d(TAG, "CartController: retrieving cart items data");
             cartItemDB = CartItemDB.with(context);
@@ -31,20 +53,23 @@ public class CartController {
             Log.d(TAG, "CartController: failed to retrieve cart items data");
             e.printStackTrace();
         }
-
-        for (int i = 0; i < cartItemList.size(); ++i) {
-            Log.d(TAG, "CartController: " + cartItemList.get(i));
-        }
     }
 
     public static CartController with(Context context) {
         return new CartController(context);
     }
 
+    public static CartController with(Context context, AtomicLong totalPrice) {
+        return new CartController(context, totalPrice);
+    }
+
     public void addToCart(CartItem cartItem) {
         try {
+            // update on database
+            cartItemDB.orderItemDao().insert(cartItem);
             cartItemList.add(cartItem);
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -53,16 +78,23 @@ public class CartController {
         return cartItemList;
     }
 
-    public int getCartListSize() {
-        try {
-            return cartItemList.size();
-        } catch (Exception e) {
-            Log.d(TAG, e.getMessage());
-            return -1;
-        }
+    // this function is just used for demo
+    public void setCartList(List<CartItem> cartItemList) {
+        // please sort cartItemList by productId
+        this.cartItemList = cartItemList;
+        this.cartItemList.sort((o1, o2) -> (int) (o1.getProductId() - o2.getProductId()));
     }
 
-    public void increaseNumItems(List<CartItem> cartItemList, int position, ChangeNumItemsListener changeNumItemsListener) {
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+        this.productList.sort((o1, o2) -> (int) (o1.getId() - o2.getId()));
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void increaseNumItems(int position, ChangeNumItemsListener changeNumItemsListener) {
         CartItem cartItem = cartItemList.get(position);
 
         // update on database
@@ -75,7 +107,7 @@ public class CartController {
         changeNumItemsListener.onChanged();
     }
 
-    public void decreaseNumItems(List<CartItem> cartItemList, int position, ChangeNumItemsListener changeNumItemsListener) {
+    public void decreaseNumItems(int position, ChangeNumItemsListener changeNumItemsListener) {
         CartItem cartItem = cartItemList.get(position);
 
         if (cartItemList.get(position).getQuantity() > 1) {
@@ -85,26 +117,22 @@ public class CartController {
             // update on code
             cartItem.setQuantity(cartItem.getQuantity() - 1);
             cartItemList.set(position, cartItem);
-        }
-        else if (cartItemList.get(position).getQuantity() == 1) {
-            // update on database
-            cartItemDB.orderItemDao().delete(cartItemList.get(position));
 
-            // update on code
-            cartItemList.remove(position);
+            changeNumItemsListener.onChanged();
         }
-        changeNumItemsListener.onChanged();
     }
 
-    public void deleteItem(List<CartItem> cartItemList, int position, ChangeNumItemsListener changeNumItemsListener) {
+    public void deleteItem(int position, ChangeNumItemsListener changeNumItemsListener) {
+        if (position == -1)
+            return;
+
         // update on database
         cartItemDB.orderItemDao().delete(cartItemList.get(position));
 
         // update on code
         cartItemList.remove(position);
+        productList.remove(position);
 
         changeNumItemsListener.onChanged();
     }
-
-
 }
--- android/app/src/main/java/com/example/jodern/fragment/CartFragment.java
+++ android/app/src/main/java/com/example/jodern/fragment/CartFragment.java
@@ -1,28 +1,45 @@
 package com.example.jodern.fragment;
 
-import android.content.Intent;
+import static com.example.jodern.Utils.vndFormatPrice;
+
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.jodern.R;
+import com.example.jodern.adapter.CartAdapter;
 import com.example.jodern.cart.CartController;
 import com.example.jodern.cart.cartitem.CartItem;
+import com.example.jodern.customwidget.MyToast;
+import com.example.jodern.interfaces.ChangeNumItemsListener;
+import com.example.jodern.model.Product;
+import com.example.jodern.provider.Provider;
 
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class CartFragment extends Fragment {
+    private final Long SHIPPING_FEE = 30000L;
+
+    public static final String TAG = "CartFragment";
     private ImageButton navbarBtn;
     private RecyclerView cartRecyclerView;
     private CartController cartController;
@@ -48,6 +65,7 @@ public class CartFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        Provider.with(this.getContext()).setCurrentFragment(TAG);
         return inflater.inflate(R.layout.fragment_cart, container, false);
     }
 
@@ -56,32 +74,11 @@ public class CartFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         if (navbarBtn != null)
             navbarBtn.setImageResource(R.drawable.ic_cart_filled);
+        cartController = CartController.with(this.getContext());
 
         initViews();
-        handleAddToCartSignal();
+//        handleAddToCartSignal();
         showCartItems();
-//        initCartLists();
-    }
-
-    private void showCartItems() {
-        List<CartItem> cartItems = cartController.getCartList();
-
-
-    }
-
-    private void handleAddToCartSignal() {
-        Bundle args = getArguments();
-        if (args == null)
-            return;
-
-        Long productId = args.getLong("productId", -1);
-        if (productId == -1)
-            return;
-
-        Integer quantity = args.getInt("quantity", -1);
-        String size = args.getString("size", "");
-        CartItem newItem = new CartItem(productId, quantity, size);
-        cartController.addToCart(newItem);
     }
 
     private void initViews() {
@@ -91,8 +88,98 @@ public class CartFragment extends Fragment {
         shippingText = getView().findViewById(R.id.cartShippingText);
         totalText = getView().findViewById(R.id.cartTotalText);
         cartEmptyWrapper = getView().findViewById(R.id.cartEmptyWrapper);
-        // TODO: card loading effect (maybe it's very hard to implement now, because we are call API inside adapter)
         cartLoadingWrapper = getView().findViewById(R.id.cartLoadingWrapper);
+    }
+
+    private void showCartItems() {
+        if (cartController.getCartList().size() == 0) {
+            cartEmptyWrapper.setVisibility(View.VISIBLE);
+            cartLoadingWrapper.setVisibility(View.GONE);
+            cartLayout.setVisibility(View.GONE);
+            return;
+        }
+
+        ArrayList<Long> productIds = new ArrayList<>();
+        for (CartItem cartItem : cartController.getCartList()) {
+            productIds.add(cartItem.getProductId());
+        }
+
+        // call API to get products
+        cartLoadingWrapper.setVisibility(View.VISIBLE);
+        String entry = "product-list";
+        String params = "id=";
+        for (int i = 0; i < productIds.size(); i++) {
+            params += String.valueOf(productIds.get(i));
+            if (i != productIds.size() - 1)
+                params += ",";
+        }
+        String url = "http://jodern.store:8000/api/" + entry + "?" + params;
+        JsonObjectRequest getRequest = new JsonObjectRequest (
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        handleResponse(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        cartLoadingWrapper.setVisibility(View.GONE);
+                        MyToast.makeText(getContext(), getString(R.string.error_message), Toast.LENGTH_SHORT);
+                    }
+                }
+        );
+        Provider.with(this.getContext()).addToRequestQueue(getRequest);
+    }
+
+    private void handleResponse(JSONObject response) {
+        cartLoadingWrapper.setVisibility(View.GONE);
+
+        // Get the information of products in cart
+        List<Product> cartProducts = Product.parseProductListFromResponse(response);
+        cartController.setProductList(cartProducts);
+
+        updateTotalPrice();
+
+        CartAdapter adapter = new CartAdapter(cartController, this.getContext(), new ChangeNumItemsListener() {
+            @Override
+            public void onChanged() {
+                updateTotalPrice();
+            }
+        });
+        cartRecyclerView.setAdapter(adapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this.getContext());
+        cartRecyclerView.setLayoutManager(layoutManager);
+
+        showCartLayout(cartProducts.isEmpty());
+    }
+
+    private void updateTotalPrice() {
+        Long subTotal = 0L;
+        List<CartItem> cartItems = cartController.getCartList();
+        List<Product> cartProducts = cartController.getProductList();
+        for (int i = 0; i < cartItems.size(); i++) {
+            subTotal += cartItems.get(i).getQuantity() * cartProducts.get(i).getPrice();
+        }
+        subTotalText.setText(vndFormatPrice(subTotal));
+        totalText.setText(vndFormatPrice(subTotal + SHIPPING_FEE));
+
+        if (subTotal == 0L) {
+            showCartLayout(true);
+        }
+    }
+
+    private void showCartLayout(boolean isEmpty) {
+        if (isEmpty) {
+            cartEmptyWrapper.setVisibility(View.VISIBLE);
+            cartLayout.setVisibility(View.GONE);
+        } else {
+            cartEmptyWrapper.setVisibility(View.GONE);
+            cartLayout.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
