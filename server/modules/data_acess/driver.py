0295c0a66c6d3496da8c07847b3d53acc98e1d43
do.tranngoc <dotrann.1412@gmail.com>
2022-12-30 20:59:28 +0000

    fix connection busy due to another command being processed

--- server/modules/data_acess/driver.py
+++ server/modules/data_acess/driver.py
@@ -10,14 +10,15 @@ class Connector:
     __connection = None
     __lastUsed = None
             
-    def establishConnection():
-        if Connector.__connection == None or datetime.datetime.now() - Connector.__lastUsed > datetime.timedelta(minutes = 15):
+    def establishConnection():        
+        if not Connector.__connection or datetime.datetime.now() - Connector.__lastUsed > datetime.timedelta(minutes = 15):
             Connector.__connection = pyodbc.connect(
                 driver = '{ODBC Driver 17 for SQL Server}' if platform.system() == 'Linux' else '{SQL Server}',
                 server=f'{config.Config.getValue("sql-server-host")},{config.Config.getValue("sql-server-port")}',
                 database=f'{config.Config.getValue("dbname")}',
                 uid=f'{config.Config.getValue("sql-server-username")}',
                 pwd=f'{config.Config.getValue("sql-server-password")}',
+                mars_connection='yes',
             )
             Connector.__lastUsed = datetime.datetime.now()
         return Connector.__connection
