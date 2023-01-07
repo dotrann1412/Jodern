470b52244d7f79cb335bc2b40dda373cae5207b1
do.tranngoc <dotrann.1412@gmail.com>
2023-01-07 03:30:57 +0000

    connection for BG jobs

--- server/modules/data_acess/driver.py
+++ server/modules/data_acess/driver.py
@@ -22,3 +22,18 @@ class Connector:
             )
             Connector.__lastUsed = datetime.datetime.now()
         return Connector.__connection
+    
+    __backgroudConnection = None
+    __backgroundLastTimeEstablished = None
+    def establishBackgroundConnection():
+        if not Connector.__backgroudConnection or datetime.datetime.now() - Connector.__backgroundLastTimeEstablished > datetime.timedelta(minutes = 15):
+            Connector.__backgroudConnection = pyodbc.connect(
+                driver = '{ODBC Driver 17 for SQL Server}' if platform.system() == 'Linux' else '{SQL Server}',
+                server=f'{config.Config.getValue("sql-server-host")},{config.Config.getValue("sql-server-port")}',
+                database=f'{config.Config.getValue("dbname")}',
+                uid=f'{config.Config.getValue("sql-server-username")}',
+                pwd=f'{config.Config.getValue("sql-server-password")}',
+                mars_connection='yes',
+            )
+            Connector.__backgroundLastTimeEstablished = datetime.datetime.now()
+        return Connector.__backgroudConnection
